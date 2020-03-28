a2c1ff0cf9a8cfef90dcce4ec2ac6c96d2f42454
user <ellen60538@gmail.com>
2020-03-28 16:08:30 +0800

    2020-03-28-16:08

---
+++ app/src/main/java/tw/org/iii/ellen/ellen21/MainActivity.java
@@ -0,0 +1,49 @@
+package tw.org.iii.ellen.ellen21;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView ;
+    private EditText max ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webView) ;
+        max = findViewById(R.id.max) ;
+        initWebView() ;
+    }
+
+    private void initWebView(){
+        webView.setWebViewClient(new WebViewClient()) ;//扮演瀏覽器,這樣就不會跳轉到網頁
+        WebSettings settings = webView.getSettings() ;
+        settings.setJavaScriptEnabled(true) ;
+        //webView.loadUrl("http://www.iii.org.tw");
+        webView.loadUrl("file:///android_asset/ellen.html");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()){
+            webView.goBack() ;
+        }else {
+            //TODO: 想先出現Toast,要連續按兩次返回才關閉APP
+            super.onBackPressed();
+        }
+    }
+
+    public void test1(View view) {
+        String strMax = max.getText().toString() ;
+        webView.loadUrl(String.format("javascript:test1(%s)",strMax)) ;
+    }
+}
